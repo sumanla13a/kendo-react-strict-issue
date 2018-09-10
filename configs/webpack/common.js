d1660aece159a011038d9a59dcd8d82f4adc6f03
sumanla13a <lamasuman2@gmail.com>
2018-09-10 11:30:00 -0700

    bug reproduction for kendo react

---
+++ configs/webpack/common.js
@@ -0,0 +1,58 @@
+// shared config (dev and prod)
+const {resolve} = require('path');
+const {CheckerPlugin} = require('awesome-typescript-loader');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: 'web',  
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  context: resolve(__dirname, '../../src'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+              babelrc: true,
+              plugins: ['react-hot-loader/babel', 'syntax-dynamic-import'],
+              cacheDirectory: true
+          },
+        },
+        {
+            loader: 'awesome-typescript-loader'
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }],
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new StyleLintPlugin(),
+    new HtmlWebpackPlugin({template: 'index.html.ejs',}),
+  ],
+  performance: {
+    hints: false,
+  },
+};
---
+++ src/custom.ts
@@ -0,0 +1,5 @@
+import { cold } from "react-hot-loader";
+import { Grid, GridColumn } from "@progress/kendo-react-grid";
+
+cold(Grid);
+cold(GridColumn);
